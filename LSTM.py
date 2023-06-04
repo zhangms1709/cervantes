49fb9d82fce2646a9d3a3a5c0b0eed5584a65c86
Mason Zhang <mzhang1709@gmail.com>
2023-06-04 19:52:27 -0400

    LSTM trained

---
+++ LSTM.py
@@ -0,0 +1,234 @@
+import tensorflow as tf
+from preprocess import get_data
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+
+BATCH_SIZE = 64
+BUFFER_SIZE = 10000
+
+dataset, vocab = get_data("data/DonQ1.txt")
+char2index = {char: index for index, char in enumerate(vocab)}
+index2char = np.array(vocab)
+
+# Let's do a quick detour and see how Embeding layer works.
+# It takes several char indices sequences (batch) as an input.
+# It encodes every character of every sequence to a vector of tmp_embeding_size length.
+tmp_vocab_size = 10
+tmp_embeding_size = 5
+tmp_input_length = 8
+tmp_batch_size = 2
+
+tmp_model = tf.keras.models.Sequential()
+tmp_model.add(tf.keras.layers.Embedding(
+  input_dim=tmp_vocab_size,
+  output_dim=tmp_embeding_size,
+  input_length=tmp_input_length
+))
+# The model will take as input an integer matrix of size (batch, input_length).
+# The largest integer (i.e. word index) in the input should be no larger than 9 (tmp_vocab_size).
+# Now model.output_shape == (None, 10, 64), where None is the batch dimension.
+tmp_input_array = np.random.randint(
+  low=0,
+  high=tmp_vocab_size,
+  size=(tmp_batch_size, tmp_input_length)
+)
+tmp_model.compile('rmsprop', 'mse')
+tmp_output_array = tmp_model.predict(tmp_input_array)
+
+print('tmp_input_array shape:', tmp_input_array.shape)
+print('tmp_input_array:')
+print(tmp_input_array)
+print()
+print('tmp_output_array shape:', tmp_output_array.shape)
+print('tmp_output_array:')
+print(tmp_output_array)
+
+# Length of the vocabulary in chars.
+vocab_size = len(vocab)
+
+# The embedding dimension.
+embedding_dim = 256
+
+# Number of RNN units.
+rnn_units = 1024
+
+def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
+    model = tf.keras.models.Sequential()
+
+    model.add(tf.keras.layers.Embedding(
+      input_dim=vocab_size,
+      output_dim=embedding_dim,
+      batch_input_shape=[batch_size, None]
+    ))
+
+    model.add(tf.keras.layers.LSTM(
+      units=rnn_units,
+      return_sequences=True,
+      stateful=True,
+      recurrent_initializer=tf.keras.initializers.GlorotNormal()
+    ))
+
+    model.add(tf.keras.layers.Dense(vocab_size))
+  
+    return model
+
+model = build_model(vocab_size, embedding_dim, rnn_units, BATCH_SIZE)
+
+model.summary()
+
+tf.keras.utils.plot_model(
+    model,
+    show_shapes=True,
+    show_layer_names=True,
+)
+
+"""## Train the model
+
+At this point the problem can be treated as a standard classification problem. Given the previous RNN state, and the input this time step, predict the class of the next character.
+
+### Attach an optimizer, and a loss function
+"""
+
+# An objective function.
+# The function is any callable with the signature scalar_loss = fn(y_true, y_pred).
+def loss(labels, logits):
+    return tf.keras.losses.sparse_categorical_crossentropy(
+      y_true=labels,
+      y_pred=logits,
+      from_logits=True
+    )
+
+adam_optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+model.compile(
+    optimizer=adam_optimizer,
+    loss=loss
+)
+
+"""### Configure checkpoints"""
+
+# Directory where the checkpoints will be saved.
+checkpoint_dir = 'tmp/checkpoints'
+os.makedirs(checkpoint_dir, exist_ok=True)
+
+# Name of the checkpoint files
+checkpoint_prefix = os.path.join(checkpoint_dir, 'ckpt_{epoch}')
+
+checkpoint_callback=tf.keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_prefix,
+    save_weights_only=True
+)
+
+"""### Execute the training"""
+
+EPOCHS=2
+
+history = model.fit(
+  x=dataset,
+  epochs=EPOCHS,
+  callbacks=[
+    checkpoint_callback
+  ]
+)
+
+# def render_training_history(training_history):
+#     loss = training_history.history['loss']
+#     plt.title('Loss')
+#     plt.xlabel('Epoch')
+#     plt.ylabel('Loss')
+#     plt.plot(loss, label='Training set')
+#     plt.legend()
+#     plt.grid(linestyle='--', linewidth=1, alpha=0.5)
+#     plt.show()
+
+# render_training_history(history)
+
+"""## Generate text
+
+### Restore the latest checkpoint
+
+To keep this prediction step simple, use a batch size of 1.
+
+Because of the way the RNN state is passed from timestep to timestep, the model only accepts a fixed batch size once built.
+
+To run the model with a different `batch_size`, we need to rebuild the model and restore the weights from the checkpoint.
+"""
+
+tf.train.latest_checkpoint(checkpoint_dir)
+
+simplified_batch_size = 1
+
+model = build_model(vocab_size, embedding_dim, rnn_units, batch_size=1)
+
+model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
+
+model.build(tf.TensorShape([simplified_batch_size, None]))
+
+model.summary()
+
+"""### The prediction loop
+
+The following code block generates the text:
+
+- It Starts by choosing a start string, initializing the RNN state and setting the number of characters to generate.
+
+- Get the prediction distribution of the next character using the start string and the RNN state.
+
+- Then, use a categorical distribution to calculate the index of the predicted character. Use this predicted character as our next input to the model.
+
+- The RNN state returned by the model is fed back into the model so that it now has more context, instead than only one character. After predicting the next character, the modified RNN states are again fed back into the model, which is how it learns as it gets more context from the previously predicted characters.
+
+![Prediction loop](https://www.tensorflow.org/tutorials/text/images/text_generation_sampling.png)
+
+Image source: [Text generation with an RNN](https://www.tensorflow.org/tutorials/text/text_generation) notebook.
+"""
+
+# num_generate
+# - number of characters to generate.
+#
+# temperature
+# - Low temperatures results in more predictable text.
+# - Higher temperatures results in more surprising text.
+# - Experiment to find the best setting.
+def generate_text(model, start_string, num_generate = 1000, temperature=1.0):
+    # Evaluation step (generating text using the learned model)
+
+    # Converting our start string to numbers (vectorizing).
+    input_indices = [char2index[s] for s in start_string]
+    input_indices = tf.expand_dims(input_indices, 0)
+
+    # Empty string to store our results.
+    text_generated = []
+
+    # Here batch size == 1.
+    model.reset_states()
+    for char_index in range(num_generate):
+        predictions = model(input_indices)
+        # remove the batch dimension
+        predictions = tf.squeeze(predictions, 0)
+
+        # Using a categorical distribution to predict the character returned by the model.
+        predictions = predictions / temperature
+        predicted_id = tf.random.categorical(
+        predictions,
+        num_samples=1
+        )[-1,0].numpy()
+
+        # We pass the predicted character as the next input to the model
+        # along with the previous hidden state.
+        input_indices = tf.expand_dims([predicted_id], 0)
+
+        text_generated.append(index2char[predicted_id])
+
+    return (start_string + ''.join(text_generated))
+
+# Generate the text with default temperature (1.0).
+print(generate_text(model, start_string=u"Don"))
+
+# Generate the text with higher temperature to get more unexpected results.
+print(generate_text(model, start_string=u"Don", temperature=1.5))
+
+"""## Save the model"""
+
+model_name = 'quijote_rnn.h5'
+model.save(model_name, save_format='h5')
--- preprocess.py
+++ preprocess.py
@@ -16,41 +16,61 @@ def get_data(file_name):
     Cleans up and prepares data for model training using regexes. 
     Splits the data into training sequences and batches.
     """
-    train = []
-    vocab = {}
-    vocab_size = 0
     seq_length = 150
-    BATCH_SIZE = 64
-    BUFFER_SIZE = 10000
+    batch_size = 64
+    buffer_size = 10000
 
     text = open(file_name, mode='r', encoding='utf-8-sig').read()
+    text = re.sub("[^\w\s]+", "", text)
 
-    for line in text:
-        line = re.sub("[^\w\s]+", "", line)
-        tokens = line.split()
-        train.extend(tokens)
-            
-        for t in tokens:
-            if t not in vocab:
-                vocab[t] = vocab_size
-                vocab_size += 1
-    vocab['UNK'] = vocab_size
-    vocab_size += 1
-
+    vocab = sorted(set(text))
+    vocab_size = len(vocab)
     char2index = {char: index for index, char in enumerate(vocab)}
 
-    text = open(file_name, mode='r', encoding='utf-8-sig').read()
+    text_as_int = np.array([char2index[char] for char in text])
 
-    # train = list(map(lambda x: vocab[x], train))
-    text_as_int = np.array([char2index.get(char, 'UNK') for char in text])
     char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
     sequences = char_dataset.batch(seq_length + 1, drop_remainder=True)
 
     dataset = sequences.map(split_input_target)
-    dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
-    # print(len(train))
-    # print(train[:15])
-    return dataset
+    dataset = dataset.shuffle(buffer_size).batch(batch_size, drop_remainder=True)
+
+    return dataset, vocab
+    # train = []
+    # vocab = {}
+    # vocab_size = 0
+    # seq_length = 150
+    # BATCH_SIZE = 64
+    # BUFFER_SIZE = 10000
+
+    # text = open(file_name, mode='r', encoding='utf-8-sig').read()
+
+    # for line in text:
+    #     line = re.sub("[^\w\s]+", "", line)
+    #     tokens = line.split()
+    #     train.extend(tokens)
+            
+    #     for t in tokens:
+    #         if t not in vocab:
+    #             vocab[t] = vocab_size
+    #             vocab_size += 1
+    # vocab['UNK'] = vocab_size
+    # vocab_size += 1
+
+    # char2index = {char: index for index, char in enumerate(vocab)}
+
+    # text = open(file_name, mode='r', encoding='utf-8-sig').read()
+
+    # # train = list(map(lambda x: vocab[x], train))
+    # text_as_int = np.array([char2index.get(char, 'UNK') for char in text])
+    # char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
+    # sequences = char_dataset.batch(seq_length + 1, drop_remainder=True)
+
+    # dataset = sequences.map(split_input_target)
+    # dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
+    # # print(len(train))
+    # # print(train[:15])
+    # return dataset, vocab
 
 
-get_data("data/DonQ1.txt")
+# get_data("data/DonQ1.txt")
